dc54d4cbbcb7c98f58f2bd50512e5fc9992e2c82
Akanimoekong <akanimoekong@gmail.com>
2022-11-30 15:48:54 +0100

    Done with Onboarding

---
+++ lib/Screens/dashboard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xFFFECDD6),
+          textTheme: TextTheme(bodyText2: TextStyle(color: Color(0xFFF63D68)))),
+      home: SafeArea(
+        child: Scaffold(
+          // backgroundColor: Color(0xFFFECDD6),
+          body: Container(
+            child: Column( children: [
+
+            ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/onboarding.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  left: 10,
+                  top: 10,
+                  bottom: 40,
+                ),
+                child: Row(children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "images/mainlogo.png",
+                    ),
+                    backgroundColor: Colors.transparent,
+                    radius: 25,
+                  ),
+                ]),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 5.0),
+                  child: Text(
+                    "Hi, Nice to meet you",
+                    style: TextStyle(color: Colors.pink),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Welcome to Hair Brace",
+                  style: TextStyle(
+                      color: Colors.pink,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: double.infinity,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: new DecorationImage(
+                      image: ExactAssetImage("images/onboardimg.png"),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 60,
+                margin: EdgeInsets.all(16),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    maximumSize: Size(double.infinity, 100),
+                    backgroundColor: Colors.pinkAccent,
+                    side: BorderSide(
+                      color: Colors.pinkAccent,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 60,
+                margin: EdgeInsets.only(
+                  right: 16,
+                  left: 16,
+                ),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    maximumSize: Size(double.infinity, 100),
+                    backgroundColor: Colors.white,
+                    side: BorderSide(
+                      color: Colors.pinkAccent,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.pink,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
