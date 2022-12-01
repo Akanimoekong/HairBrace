0207333c2b1941a8cf59a8fb7fef731cbef072af
Akanimoekong <akanimoekong@gmail.com>
2022-12-01 10:52:31 +0100

    Worked on the font on the SplashScreen

--- lib/Screens/splashscreen.dart
+++ lib/Screens/splashscreen.dart
@@ -8,8 +8,26 @@ class SplashScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Image.asset("images/mainlogo.png"),
-        ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                "images/mainlogo.png",
+              ),
+            ),
+            Center(
+              child: Text(
+                "The essence of hair",
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontFamily: 'Post No Bills Colombo',
+                  fontSize: 22
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
